e1c26246efcef45fb5da97afc09dce946cbc1a86
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-17 22:57:40 +0100

    create an updateAJob function and its patch route, use object keys to secure dynamic partial updates depending on the user's choice

--- controllers/jobTrackController.js
+++ controllers/jobTrackController.js
@@ -47,5 +47,39 @@ const retriveAParticularJob = async (req,res) => {
     }
 }
 
+const updateAJob = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const updates = req.body;
 
-module.exports = { createAJob, retriveJobs, retriveAParticularJob }
+        if (Object.keys(updates).length === 0) {
+            return res.status(400).json({ message: "No fields provided for update" });
+        }
+
+        if (updates.status) {
+            const allowedStatuses = ["Applied", "Interviewing", "Offer", "Rejected", "Ghosted"];
+            if (!allowedStatuses.includes(updates.status)) {
+                return res.status(400).json({ message: `Invalid status. Allowed: ${allowedStatuses.join(", ")}` });
+            }
+        }
+
+        const fields = Object.keys(updates).map((key, index) => `${key} = $${index + 1}`).join(", ");
+        const values = Object.values(updates);
+        values.push(id);
+
+        const query = `UPDATE jobs SET ${fields}, updated_at = CURRENT_TIMESTAMP WHERE id = $${values.length} RETURNING *`;
+        const response = await db.query(query, values);
+
+            if (response.rows.length === 0) {
+                return res.status(404).json({ message: "Job not found" });
+            }
+    
+            res.status(200).json({ message: "Job updated successfully", job: response.rows[0]})
+    } catch (error) {
+        console.error("Error retrieving a chosen job:", error);
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
+}
+
+
+module.exports = { createAJob, retriveJobs, retriveAParticularJob, updateAJob }
--- routes/jobTrackRoutes.js
+++ routes/jobTrackRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createAJob, retriveJobs, retriveAParticularJob } = require("../controllers/jobTrackController");
+const { createAJob, retriveJobs, retriveAParticularJob, updateAJob } = require("../controllers/jobTrackController");
 
 router.post("/jobs", createAJob);
 router.get("/jobs", retriveJobs);
-router.get("/jobs/:id", retriveAParticularJob)
+router.get("/jobs/:id", retriveAParticularJob);
+router.patch("/jobs/:id", updateAJob)
 
 
 
