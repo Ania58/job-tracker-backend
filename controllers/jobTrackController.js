5f7fc22909fa5205dc5f623b0df2de49bbba3ca8
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-17 22:07:46 +0100

    create a retrive a particular job function and a get route

--- controllers/jobTrackController.js
+++ controllers/jobTrackController.js
@@ -25,5 +25,21 @@ const retriveJobs = async (req,res) => {
     }
 }
 
+const retriveAParticularJob = async (req,res) => {
+    try {
+        const {id} = req.params;
+        const response = await db.query(`SELECT * FROM jobs WHERE id = $1`, [id]);
 
-module.exports = { createAJob, retriveJobs }
+        if (response.rows.length === 0) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+
+        res.status(200).json({job: response.rows[0]})
+    } catch (error) {
+        console.error("Error retrieving a chosen job:", error);
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
+}
+
+
+module.exports = { createAJob, retriveJobs, retriveAParticularJob }
--- routes/jobTrackRoutes.js
+++ routes/jobTrackRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createAJob, retriveJobs } = require("../controllers/jobTrackController");
+const { createAJob, retriveJobs, retriveAParticularJob } = require("../controllers/jobTrackController");
 
 router.post("/jobs", createAJob);
 router.get("/jobs", retriveJobs);
+router.get("/jobs/:id", retriveAParticularJob)
 
 
 
