2883a886593430a7e94689f5774ac5cae34a14a5
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-17 21:50:46 +0100

    create a function and a get route to retrive all the jobs

--- controllers/jobTrackController.js
+++ controllers/jobTrackController.js
@@ -15,5 +15,15 @@ const createAJob = async (req,res) => {
     
 };
 
+const retriveJobs = async (req,res) => {
+    try {
+        const response = await db.query("SELECT * FROM jobs")
+        res.status(200).json({job: response.rows[0]})
+    } catch (error) {
+        console.error("Error retrieving jobs:", error);
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
+}
 
-module.exports = { createAJob }
+
+module.exports = { createAJob, retriveJobs }
--- routes/jobTrackRoutes.js
+++ routes/jobTrackRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createAJob } = require("../controllers/jobTrackController");
+const { createAJob, retriveJobs } = require("../controllers/jobTrackController");
 
 router.post("/jobs", createAJob);
+router.get("/jobs", retriveJobs);
 
 
 
