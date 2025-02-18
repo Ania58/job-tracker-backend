4a41798208262b330f56002bc013bb371728f1d5
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-18 15:56:26 +0100

    apply the middleware to the routes that need protection

--- routes/jobTrackRoutes.js
+++ routes/jobTrackRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { createAJob, retriveJobs, retriveAParticularJob, updateAJob, deleteAJob } = require("../controllers/jobTrackController");
+const requireAuth = require("../middleware/authMiddleware");
 
-router.post("/jobs", createAJob);
-router.get("/jobs", retriveJobs);
+router.post("/jobs",  requireAuth, createAJob);
+router.get("/jobs",  requireAuth, retriveJobs);
 router.get("/jobs/:id", retriveAParticularJob);
 router.patch("/jobs/:id", updateAJob);
 router.delete("/jobs/:id", deleteAJob);
