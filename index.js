7035333d3c41eff82d6e5a9241766b2a084217bf
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-19 13:32:24 +0100

    enable cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -14,6 +15,7 @@ const jobRoutes = require("./routes/jobTrackRoutes");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use(
     session({
       secret: process.env.SESSION_SECRET,
