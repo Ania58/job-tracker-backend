9079311c99d67f43c9c8aa9154a9fff66bdd113a
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-19 15:43:30 +0100

    modify db to allow deployment

--- config/db.js
+++ config/db.js
@@ -3,14 +3,21 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const db = new pg.Client({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-    ssl: process.env.HOST !== 'localhost' ? { rejectUnauthorized: false } : false
-});
+const db = new pg.Client(
+    process.env.DATABASE_URL
+    ? {
+          connectionString: process.env.DATABASE_URL,
+          ssl: { rejectUnauthorized: false },
+      }
+    : {
+          user: process.env.USER,
+          host: process.env.HOST,
+          database: process.env.DATABASE,
+          password: process.env.PASSWORD,
+          port: process.env.PORT,
+          ssl: process.env.HOST !== "localhost" ? { rejectUnauthorized: false } : false,
+      }
+);
 
 db.connect();
 
