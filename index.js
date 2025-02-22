6427a37d8804f33d5cedd0010c806c278150136c
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-22 15:39:15 +0100

    adjust for local testing

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(
       cookie: {
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        sameSite: "none",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 24 * 60 * 60 * 1000,
     },
     })
