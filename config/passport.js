a2418f36d5d2205084f2dfeca357d139a6d2313d
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-22 20:32:53 +0100

    come back to the previous version of google strategy to see if errors persist

--- config/passport.js
+++ config/passport.js
@@ -27,7 +27,7 @@ passport.use(
   })
 );
 
-passport.use(
+/*passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
@@ -58,6 +58,33 @@ passport.use(
         return done(err);
       }
     }
+  )
+);*/
+
+passport.use(
+  new GoogleStrategy(
+      {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+          try {
+              const result = await db.query("SELECT * FROM users WHERE google_id = $1", [profile.id]);
+
+              if (result.rows.length === 0) {
+                  const newUser = await db.query(
+                      "INSERT INTO users (email, google_id) VALUES ($1, $2) RETURNING *",
+                      [profile.email, profile.id]
+                  );
+                  return done(null, newUser.rows[0]);
+              }
+
+              return done(null, result.rows[0]);
+          } catch (err) {
+              return done(err);
+          }
+      }
   )
 );
 
