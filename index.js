b5672832596b814ac6800cb827f614eae316aa26
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-02-22 14:56:03 +0100

    test for cookie preservation

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: process.env.NODE_ENV === "production" || req.secure,
+        secure: process.env.NODE_ENV === "production",
         httpOnly: true,
         sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
